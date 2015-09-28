0101df3db85e98dbebb06393a6d5a6f72675d25f
vishnupriyam <mvpnov1994@gmail.com>
2015-09-28 20:23:55 +0530

    Adds abstracttree.h

---
+++ absracttree.h
@@ -0,0 +1,19 @@
+struct ASTNode{
+       struct TypeTable *Type;
+       int Nodetype;
+       char *Name;
+       union Constant Value;
+       struct Tnode *Arglist;
+       struct Tnode *Ptr1,*Ptr2,*Ptr3;
+       struct GSymbol *Gentry;
+       struct LSymbol *Lentry;
+}AST;
+
+union Constant {
+	int intval;
+	string strval;
+};
+
+AST* Treecreate(TypeTable *type,int nodetype,char *name,int value,AST *arglist,AST *t1,AST *t2,AST *t3);
+AST* TreeAppend(AST *t,AST *t1, AST *t2, AST *t3);
+memstruct interpret(AST *t);
