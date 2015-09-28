7289a4a54e6c2849b514d1943f94a718ed9c3751
vishnupriyam <mvpnov1994@gmail.com>
2015-09-28 22:39:24 +0530

    Fix argument error in TreeCreate defnition

--- abstracttree.h
+++ abstracttree.h
@@ -23,6 +23,6 @@ typedef union memstruct{
 }memstruct;
 
 
-AST* Treecreate(TypeTable *type,int nodetype,char *name,int value,AST *arglist,AST *t1,AST *t2,AST *t3);
+AST* Treecreate(TypeTable *type,int nodetype,char *name,union Constant value,AST *arglist,AST *t1,AST *t2,AST *t3);
 AST* TreeAppend(AST *t,AST *t1, AST *t2, AST *t3);
 memstruct interpret(AST *t);
